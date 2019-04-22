30cd036735ffa69ce0423b8917354e2182000eb5
moussaab moulim <moussaabmma@gmail.com>
2019-04-22 13:20:26 +0100

    Updates

---
+++ precache-manifest.658bafc9d6cb3327ab6f57d24ec58643.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "fc32eec6282e7a47fd7f6f6d821fef51",
+    "url": "/static/media/home.fc32eec6.png"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "d47341446fb6c16db935",
+    "url": "/static/js/main.bef9a401.chunk.js"
+  },
+  {
+    "revision": "9a1ccfb20758c85fb2b5",
+    "url": "/static/js/2.8cad79b2.chunk.js"
+  },
+  {
+    "revision": "d47341446fb6c16db935",
+    "url": "/static/css/main.9a0c54c7.chunk.css"
+  },
+  {
+    "revision": "9a1ccfb20758c85fb2b5",
+    "url": "/static/css/2.b8001f64.chunk.css"
+  },
+  {
+    "revision": "bd0c351a85afb7b4ce3757eb9c6566c3",
+    "url": "/index.html"
+  }
+];
--- src/components/PreLoader/index.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import loaderSrc from "../../assets/preloader/preloader.gif";
-const PreLoader = (props) => (
-  <div>
-    <img style={{ width: 50 }} alt="loading gif" src={loaderSrc} />
-  </div>
-)
-
-export default PreLoader
