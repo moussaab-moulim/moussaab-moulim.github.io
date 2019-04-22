cf6c1b221df0f36823dd0375da07b810ceb1e8a6
moussaab moulim <moussaabmma@gmail.com>
2019-04-22 13:08:45 +0100

    making home section

---
+++ src/components/Home/index.js
@@ -0,0 +1,42 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.css';
+import './Home.css';
+import Button from '../Button';
+
+
+const Home = (props) => (
+    <div className="section" id="home">
+
+
+        <div id="home-bg-video" >
+
+        </div>
+
+        <div id="home-overlay">
+
+        </div>
+
+        <div id="home-content">
+            <div id="home-content-inner" className="text-center">
+                <div id="home-heading">
+                    <h1><span id="home-heading-1">Moussaab MOULIM</span><br/><span id="home-heading-2">Software <span>ENGINEER</span></span></h1>
+                </div>
+                <div id="home-text">
+                    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit consectetur adipisicing elit. Eos at
+                quam.</p>
+                </div>
+                <Button buttonClass="btn-home" />
+            </div>
+
+        </div>
+
+
+        <a href="#about" id="arrow-down">
+            <i class="fas fa-angle-down"></i>
+        </a>
+
+
+    </div>
+)
+
+export default Home;
---
+++ src/components/Main/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+
+import './index.css';
+
+import Home from '../Home';
+
+
+class Main extends Component {
+  render() {
+    return (
+      <div className="main" >
+
+<Home/>
+        
+
+      </div>
+    );
+  }
+}
+
+export default Main;
